6c492217b7185c45c43faa998e74653dbd9f9c5f
r1d3rzz <myintthwaykhine127@gmail.com>
2025-03-15 17:46:58 +0630

    admin: separate ui components

---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<div class="app-wrapper-footer">
+    <div class="app-footer">
+        <div class="app-footer__inner">
+            <div class="app-footer-left">
+                <span>Copyright {{date("Y")}}. All right reserved by Rider Pay</span>
+            </div>
+            <div class="app-footer-right">
+                <span>Developed by Rider</span>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/admin_web.php
+++ routes/admin_web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix("admin")->namespace("Backend")->middleware("auth:admin_user")->group(function () {
-    Route::get("/", "PageController@home");
+Route::prefix("admin")->name("admin.")->namespace("Backend")->middleware("auth:admin_user")->group(function () {
+    Route::get("/", "PageController@home")->name("home");
 });
